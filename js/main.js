034b8330936dde923b8ebf8231c02eeaaa01cbb9
kandanai-kwn <kandanai.ka@ku.th>
2025-12-30 11:07:19 +0700

    add js

---
+++ js/main.js
@@ -0,0 +1,85 @@
+// // // // console.log("Hello Word")
+ // // console.log("this is a teat javascript flie")
+
+
+// // // 
+// // let fname="john";// string
+// // console.log("Name:",fname);
+// // const PI=3.14;
+
+
+// // // number
+// // let age=30;//number
+// // let height=5.9;//number
+
+// // fname= "Alice"
+// // console.log("Name:",fname);
+
+
+// // fname="Bob"
+// // console.log("Name:",fname);
+// // console.log("Age:",age)
+// // console.log("Height:",height);
+
+
+// // let number="10";
+// // let number2="3";
+
+// // let result1=number+number2;
+// // console.log("ผลบวก =",result1);
+
+
+// // let number1=23;
+// // let number2=21;
+// // let condition= number1 < number2;
+// // console.log("condition:",condition);
+
+// // let number1=3;
+// // let number2=5;
+// // grade
+// // >=80 เป็นเกรด a
+// // >=70 เป็นเกรด B
+
+
+// let score=prompt("ใส่คะแนนของคุณ");
+// //if-else
+// if(score >= 80){
+//     console.log("grade= A");
+// }else if(score>=70){
+//     console.log("grade = B");
+// }
+// else if (score>=60){
+//     console.log("grade = C");
+// }else if (score>=50){
+//     console.log("grade =D");
+
+// }else{
+//     console.log("grade = F");
+// }
+// let  number1=5;
+// let number2=10;
+// let condition1=(number1>0) || (number2>0)
+// console.log("condition1:",condition1);
+
+// let age=25;
+// let gender= "famale"
+// if (age >= 18 && gender == "famale"){
+//     console.log("คุณไม่สารถเข้าร่วมได้");
+// }
+
+// let number1 = 20
+// if (!(number1 % 2 == 0)){
+//     console.log("Even")
+// }else{
+//     console.log("obb")
+// }
+
+
+let conter =0;
+while (conter <= 4){
+    conter=conter+1;
+    console.log("While: ",conter);
+}
+for (let i=0; i <= 4; i=i+1){
+    console.log("for:",i)
+}
