a1e02d8c739bed46e99f16c9c90be4051ff825d9
kandanai-kwn <kandanai.ka@ku.th>
2026-02-17 11:39:57 +0700

    add nodemon

--- blackend/index.js
+++ blackend/index.js
@@ -1,13 +1,144 @@
-const http = require('http');
-const host = 'localhost';
-const port = 8000;
 
-const requestLinstener = function(req, res) {
-    res.writeHead(200);
-    res.end('Hello World 1\n');
-};
+// // const express = require('express');
+// // const bodyParser = require('body-parser');
+// // const app = express();
 
-const server =  http.createServer(requestLinstener);
-server.listen(port, host, () => {
-    console.log(`Server running at http://${host}:${port}/`);
+// // const port = 8000
+
+// // app.use(bodyParser.json());
+
+// // let users = []
+
+// // // path = GET /test
+// // app.get('/test', (req, res) => {
+// //     let user ={
+// //         name: 'John Doe',
+// //         age: 30,
+// //         email: 'john.doe@example.com'
+// //     }
+// //     res.json(user);
+// // });
+
+// // // path = POST /user
+// // app.post('/user', (req, res) => {
+// //     let user = req.body; 
+// //     users.push(user);
+// //     res.json({
+// //         message: 'User added successfully',
+// //         user: user
+// //     });
+// // });
+
+
+// // app.listen(port, () => {
+// //     console.log(`Server is running on port ${port}`)
+// // });
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const app = express();
+
+// const port = 8000
+
+// app.use(bodyParser.json());
+
+// let users = []
+
+// // path = GET /test
+// app.get('/test', (req, res) => {
+//     let user ={
+//         name: 'John Doe',
+//         age: 30,
+//         email: 'john.doe@example.com'
+//     }
+//     res.json(user);
+// });
+// app.get('/users', (req, res) => {
+//     res.json(users);
+// });
+// // path = POST /user
+// app.post('/user', (req, res) => {
+//     let user = req.body; 
+//     users.push(user);
+//     res.json({
+//         message: 'User added successfully',
+//         user: user
+//     });
+// });
+
+
+// app.listen(port, () => {
+//     console.log(`Server is running on port ${port}`)
+// });
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+const port = 8000
+
+app.use(bodyParser.json());
+
+let users = []
+let counter = 1;
+
+// path = GET /test
+app.get('/test', (req, res) => {
+    let user ={
+        name: 'John Doe',
+        age: 30,
+        email: 'john.doe@example.com'
+    }
+    res.json(user);
+});
+app.get('/users', (req, res) => {
+    res.json(users);
+});
+// path = POST /user
+app.post('/user', (req, res) => {
+    let user = req.body;
+    user.id = counter
+    counter +=1; 
+    users.push(user);
+    res.json({
+        message: 'User added successfully',
+        user: user
+    });
+});
+
+// path = PUT /user/:id
+app.patch('/user/:id', (req, res) => {
+    let id = req.params.id;
+    let updateUser =req.body;
+    //หา  user จาก id ที่ส่งมา
+    let seletedIndex = users.findIndex(user => user.id == id);
+
+
+    //อัพเดตข้อมูล user
+   if (updateUser.name){
+        users[seletedIndex].name = updateUser.name;
+   }
+   if (updateUser.email){
+    users[seletedIndex].email = updateUser.email;
+   }
+
+
+    //เอาข้อมูลที่ update ส่ง response กลับไป
+    res.json({
+        message:'Userupdate successfully',
+        data:{
+            users:updateUser,
+            indexUpdate: seletedIndex
+        }
+    })
+
+
+});
+app.delete('/user/:id',(req , res) => {
+    let id = req.params.id;
+
+    let seletedIndex = users.findIndex(user => user.id == id);
+    users.splice(seletedIndex,1);
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
 });
