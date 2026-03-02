c888fcc821621ed9dd845d7fadfe1224b03222dd
kandanai-kwn <kandanai.ka@ku.th>
2026-03-02 17:05:11 +0700

    add 2/3

--- Frontent/index.js
+++ Frontent/index.js
@@ -1,11 +1,17 @@
-function submitData () {
+const submitData = async ()=> {
   let firstNameDOM = document.querySelector('input[name=firstname]');
   let lastNameDOM = document.querySelector('input[name=lastname]');
   let ageDOM = document.querySelector('input[name=age]');
   let genderDOM = document.querySelector('input[name=gender]:checked');
-  let interestDOMs = document.querySelectorAll('input[name=interest]:checked');
+  let interestDOMs = document.querySelectorAll('input[name=interests]:checked');
   let descriptionDOM = document.querySelector('textarea[name=description]');
 
+  let messageDOM = document.getElementById('message')
+  
+  
+  
+  
+  try {
   let interest = ''
   for (let i = 0; i < interestDOMs.length; i++) {
     interest += interestDOMs[i].value
@@ -20,8 +26,21 @@ function submitData () {
     age: ageDOM.value,
     gender: genderDOM.value,
     description: descriptionDOM.value,
-    interest: interest
+    interests: interest
+  }
+    const response = await axios.post('http://localhost:8000/users',userData)
+    console.log('response',response)
+    messageDOM.innerText='บันทึกข้อมูลสำเร็จ'
+    messageDOM.className='message success'
+    console.log('submitData',userData);
+    
+  } catch (error) {
+    if(error.response){
+      console.log('Error respose',error.response.data.message);
+    }
+    messageDOM.innerText='การบัญทึกข้อมูลผิดพลาด'
+    messageDOM.className='message danger'
+    
   }
 
-  console.log('submitData', userData);
 }
