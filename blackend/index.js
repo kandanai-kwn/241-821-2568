875700309d6b1209e1d8081779e18ce492923ba7
kandanai-kwn <kandanai.ka@ku.th>
2026-03-10 11:18:14 +0700

    add validate

--- blackend/index.js
+++ blackend/index.js
@@ -68,19 +68,52 @@ app.get('/test', (req, res) => {
 app.get('/users', (req, res) => {
     res.json(users);
 });
+
+const validateData = (userData) => {
+    let errors = [];
+    if (!userData.firstname) {
+        errors.push('กรุณากรอกชื่อ');
+    }
+    if (!userData.lastname) {
+        errors.push('กรุณากรอกนามสกุล');
+    }
+    if (!userData.age) {
+        errors.push('กรุณากรอกอายุ');
+    }
+    if (!userData.gender) {
+        errors.push('กรุณาเลือกเพศ');
+    }
+    if (!userData.interests) {
+        errors.push('กรุณาเลือกความสนใจอย่างน้อย 1 อย่าง');
+    }
+    if (!userData.description) {
+        errors.push('กรุณากรอกคำอธิบายเกี่ยวกับตัวคุณ');
+    }
+    return errors;
+}
 // path = POST /user
 app.post('/users', async (req, res) => {
     try {
-    let user = req.body;
-    const result = await conn.query('INSERT INTO users SET ?', user);
-    console.log('result',result)
-    res.json({
-        message: 'User added successfully',
-        data: result[0]
+        let user = req.body;
+        const errors = validateData(user);
+        if (errors.length > 0){
+            throw{
+                message: 'กรอกข้อมูลไม่ครบถ้วน',
+                errors:errors
+            }
+
+        }
+        const result = await conn.query('INSERT INTO users SET ?', user);
+        console.log('result',result)
+        res.json({
+            message: 'User added successfully',
+            data: result[0]
     });
 
     } catch (error) {
-        console.error('Error',error);
+        const errorMessage = error.message || 'Error creating user' ; 
+        const errors = error.errors || [];
+        console.error('Error creating',error.message);
         res.status(500).json({
             message: 'Error adding user',
             error: error
